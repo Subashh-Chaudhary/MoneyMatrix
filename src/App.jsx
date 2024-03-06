92d0409a0d263673e8af6d5b67d07b19fec63772
Subashh-Chaudhary <chysubash15963@gmail.com>
2024-03-06 21:05:58 +0545

    Responsiveness feature added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { InputBox, CurrencyDetails } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import CountryData from "./currencyData";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function App() {
   const [amount, setAmount] = useState(0);
@@ -57,16 +58,6 @@ function App() {
     setConvertedAmount(res.toFixed(2));
   };
 
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    const countryData = CountryData[country];
-    if (countryData) {
-      setSelectedCountry(countryData.country);
-    } else {
-      setSelectedCountry("Country not found");
-    }
-  };
-
   return (
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
@@ -74,10 +65,19 @@ function App() {
         backgroundImage: `url('https://images.pexels.com/photos/8370426/pexels-photo-8370426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
       }}
     >
+      <a
+        href="#"
+        onClick={() => window.location.reload()}
+        className="absolute top-6 md:top-3  left-3"
+      >
+        <h1 className="font-extrabold text-blue-100 font-archivo text-2xl lg:text-4xl self-start">
+          MoneyMatrix
+        </h1>
+      </a>
       <div className="w-full">
-        <div className=" lg:w-1/2 sm:w-96 h-auto mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+        <div className=" sm:w-1/2 h-auto mx-2 sm:mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <div className="mb-3 ">
-            <p className="text-xl text-white font-extrabold uppercase">
+            <p className="text-md lg:text-xl text-white font-extrabold uppercase">
               Conversion Date : {date}
             </p>
           </div>
@@ -99,7 +99,7 @@ function App() {
             <div className="relative w-full h-0.5">
               <button
                 type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 lg:px-4 lg:py-1 lg:text-xl"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 lg:px-4 lg:py-1 lg:text-xl text-sm"
                 onClick={swap}
               >
                 Swap
@@ -117,7 +117,7 @@ function App() {
             </div>
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white py-2 md:py-4 rounded-lg lg:text-2xl font-bold"
+              className="w-full bg-blue-600 text-white py-2 md:py-4 rounded-lg lg:text-2xl font-bold text-md"
               onClick={convert}
             >
               Convert {from.toUpperCase()} to {to.toUpperCase()}
@@ -134,6 +134,17 @@ function App() {
           </form>
         </div>
       </div>
+      <a
+        href="https://github.com/Subashh-Chaudhary/MoneyMatrix"
+        target="_blank"
+      >
+        <div className="text-blue-100 absolute bottom-20 right-4 lg:bottom-10 lg:right-12 flex gap-2 items-center">
+          <GitHubIcon fontSize="large" />
+          <p className="lg:text-xl  text-[13px] md:text-lg text-blue-100 font-archivo font-extrabold">
+            GitHub Repository
+          </p>
+        </div>
+      </a>
     </div>
   );
 }
--- src/components/CurrencyDetails.jsx
+++ src/components/CurrencyDetails.jsx
@@ -10,13 +10,14 @@ function CurrencyDetails({
   onCodeChange,
   disabled = true,
 }) {
-
   return (
-    <div className="mt-3 bg-white p-3 rounded-lg text-sm md:text-xl flex gap-4">
-      <div className="md:w-2/6">
-        <label className="text-slate-700 font-extrabold">Country Name:</label>
+    <div className="mt-3 bg-white p-3 rounded-lg text-sm md:text-xl flex gap-2 lg:gap-4">
+      <div className="w-2/6">
+        <label className="text-slate-700 font-extrabold text-[12px] lg:text-xl">
+          Country Name:
+        </label>
         <select
-          className="outline-none w-full bg-slate-200 py-2 rounded-md px-1 mt-2 text-md"
+          className="outline-none w-full bg-slate-200 py-2 rounded-md px-1 mt-2 text-[13px] lg:text-xl"
           value={country}
           onChange={(e) => onCountryChange && onCountryChange(e.target.value)}
         >
@@ -27,21 +28,25 @@ function CurrencyDetails({
           ))}
         </select>
       </div>
-      <div className="md:w-1/6">
-        <label className="text-slate-700 font-extrabold">Code:</label>
+      <div className="w-1/6">
+        <label className="text-slate-700 font-extrabold text-[12px] lg:text-xl">
+          Code:
+        </label>
         <input
           type="text"
-          className="outline-none w-full bg-slate-200 py-2 rounded-md px-1 mt-2 text-md"
+          className="outline-none w-full bg-slate-200 py-2 rounded-md px-1 mt-2 text-[13px] lg:text-xl"
           value={code}
           onChange={(e) => onCodeChange && onCodeChange(e.target.value)}
           disabled={disabled}
         />
       </div>
-      <div className="md:w-3/6">
-        <label className="text-slate-700 font-extrabold">Currency Name:</label>
+      <div className="w-3/6">
+        <label className="text-slate-700 font-extrabold text-[12px] lg:text-xl">
+          Currency Name:
+        </label>
         <input
           type="text"
-          className="outline-none w-full bg-slate-200 py-2 rounded-md px-1 mt-2 text-md"
+          className="outline-none w-full bg-slate-200 py-2 rounded-md px-1 mt-2 text-[13px] lg:text-xl"
           value={currency}
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
           disabled={disabled}
